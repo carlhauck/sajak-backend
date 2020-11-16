4660c2a6a2fd743f496a834a42b9e74fa5371b05
carlhauck <carlhauck@gmail.com>
2020-11-16 16:22:52 -0600

    adds routes to controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    
+    post "/games" => "games#create"
+
+    get "/high_scores" => "high_scores#index"
+    post "/high_scores" => "high_scores#create"
+    
+  end
+
 end
