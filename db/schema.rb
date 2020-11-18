bad5fd42bef6edf2ad50a838a35847cc3d98537e
carlhauck <carlhauck@gmail.com>
2020-11-17 18:05:04 -0600

    resets db with simplified schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_224329) do
+ActiveRecord::Schema.define(version: 2020_11_18_000253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "games", force: :cascade do |t|
-    t.integer "score"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "high_scores", force: :cascade do |t|
     t.string "player"
