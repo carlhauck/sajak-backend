06079c640b8de4254ae6028b52e6680a405c0ae6
carlhauck <carlhauck@gmail.com>
2020-11-17 18:58:08 -0600

    removes final forward slash from netlify url in cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'https://wizardly-meitner-511da3.netlify.app/', 'https://sajakhorseman.com'
+    origins 'localhost:8080', 'https://wizardly-meitner-511da3.netlify.app', 'https://sajakhorseman.com'
 
     resource '*',
       headers: :any,
